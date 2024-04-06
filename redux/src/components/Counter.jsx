7b0d88d71d4b5c5855059aea74e900ac1c5e3f8c
YunusOrak <yunusorak08@hotmail.com>
2024-04-06 11:52:09 +0300

    redux loading...

--- redux/src/components/Counter.jsx
+++ redux/src/components/Counter.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector((state) => state.counter);
+  const showcount = useSelector((state) => state.showCounter);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+  const increaseHandler = () => {
+    dispatch({ type: "INCREASE", amount: 5 });
+  };
+  const showCounter = () => {
+    dispatch({ type: "TOGGLE" });
+  };
 
   return (
     <main>
+      {showcount}
       <hr />
       <h3>Redux Counter</h3>
-      <b> 0 </b> <br />
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <b> {showcount && counter} </b> <br />
+      <button onClick={incrementHandler}>Increase</button>
+      <button onClick={increaseHandler}>Increase by 5</button>
+      <button onClick={decrementHandler}>Decrease</button> <br />
+      <button onClick={showCounter}>Show Counter</button>
       <p>
         Edit <code>src/Components/Counter.jsx</code> and save to test HMR
       </p>
--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -1,31 +1,40 @@
-const redux = require("redux");
+import { createStore } from "redux";
 
-const initialState = { counter: 0 };
+const initialState = { counter: 0, showCounter: false };
 
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return { counter: state.counter + 1 };
+      return { ...state, counter: state.counter + 1 };
       break;
     case "DECREMENT":
-      return { counter: state.counter - 1 };
+      return { ...state, counter: state.counter - 1 };
+      break;
+    case "INCREASE":
+      return { ...state, counter: state.counter + action.amount };
+      break;
+    case "TOGGLE":
+      return { counter: state.counter, showCounter: !state.showCounter };
       break;
 
     default:
+      return state;
       break;
   }
 };
 
-const store = redux.createStore(counterReducer);
+const store = createStore(counterReducer);
+
+export default store;
 
 // console.log(store.getState());
 
-const counterSubscriber = () => {
-  const latestState = store.getState();
-  console.log(latestState);
-};
+// const counterSubscriber = () => {
+//   const latestState = store.getState();
+//   console.log(latestState);
+// };
 
-store.subscribe(counterSubscriber); // Subscribe ile Durum güncellemesi dinleme
+// store.subscribe(counterSubscriber); // Subscribe ile Durum güncellemesi dinleme
 
-store.dispatch({ type: "INCREMENT" });
-store.dispatch({ type: "DECREMENT" });
+// store.dispatch({ type: "INCREMENT" });
+// store.dispatch({ type: "DECREMENT" });
