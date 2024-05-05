9c094ad0be23445b0237d54fbbee382752d9a5ca
yunuso-me <40091165+yunusorak@users.noreply.github.com>
2024-05-05 09:12:29 +0300

    Update firebase.js

--- firebase-auth/src/firebase/firebase.js
+++ firebase-auth/src/firebase/firebase.js
@@ -5,16 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDeFlyGmus-ETCP8BMy_DRZLyacyhdd980",
-  authDomain: "denemeauth-17c98.firebaseapp.com",
-  databaseURL: "https://denemeauth-17c98-default-rtdb.firebaseio.com",
-  projectId: "denemeauth-17c98",
-  storageBucket: "denemeauth-17c98.appspot.com",
-  messagingSenderId: "808530128192",
-  appId: "1:808530128192:web:ab611985d1a27b5cbc5ea5",
-  measurementId: "G-QDMMQXZYCL",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
